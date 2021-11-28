68b9866e089972a70de3488b4e012912e54446cf
林云富 <lin13177896353@163.com>
2021-11-28 22:27:22 +0800

    post

--- egg-store/app/controller/post.js
+++ egg-store/app/controller/post.js
@@ -4,7 +4,12 @@ module.exports = app => {
       console.log(1111)
 
       //this.ctx.csrf  用户访问这个页面的时候生成一个密钥
-
+      this.ctx.cookies.set('aaaa','bbbb',{
+        maxAge:24*3600*3600,
+        signed:true,
+        encrypt:true,
+        httpOnly:true
+      })
       await this.ctx.render('post');
     }
     async add() {        
