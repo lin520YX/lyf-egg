156b57adfb2bb260316b6426d6ac9daae24f46ce
林云富 <lin13177896353@163.com>
2021-12-04 22:45:38 +0800

    列表查询

--- egg-store/app/controller/admin/manager.js
+++ egg-store/app/controller/admin/manager.js
@@ -4,19 +4,16 @@ module.exports = app => {
  
   class Controller extends BaseController {
     async index(){
-      var result=await this.ctx.model.Admin.aggregate([{
-
+      var list=(await this.ctx.model.Admin.aggregate([{
         $lookup:{
           from:'role',
           localField:'role_id',
           foreignField:'_id',
           as:'role'
-  
         }      
-      }])
+      }]))||[]
       await this.ctx.render('admin/manager/index',{
-  
-        list:result
+        list
       });
     }
     async add(){
