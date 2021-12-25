a7d8718287ddbc48f5042d681acd312945be70b1
林云富 <lin13177896353@163.com>
2021-12-25 22:56:29 +0800

    多图上传

--- egg-store/app/controller/admin/focus.js
+++ egg-store/app/controller/admin/focus.js
@@ -6,12 +6,15 @@ const fs=require('fs');
     async index(){
       await this.ctx.render('/admin/focus/index')
     }
-    async multi(){}
+    async multi(){
+      await this.ctx.render('/admin/focus/multi')
+    }
     async doSingleUpload(){
       const stream = await this.ctx.getFileStream();
-      // 目标文件
+      // 目标文件 创建一个空文件
       const target = 'app/public/admin/upload/' + path.basename(stream.filename);
       const writeStream = fs.createWriteStream(target);
+      // 写入文件内容
       await pump(stream,writeStream); 
       this.ctx.body = {
         url: target,      
