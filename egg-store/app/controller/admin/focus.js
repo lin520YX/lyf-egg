1ddc604d3250055bb2be45f2591020e2de583f3d
林云富 <lin13177896353@163.com>
2021-12-25 22:42:32 +0800

    但文件上传

--- egg-store/app/controller/admin/focus.js
+++ egg-store/app/controller/admin/focus.js
@@ -1,10 +1,23 @@
 module.exports = app => {
+  const pump = require('mz-modules/pump');
+  const path=require('path');
+const fs=require('fs');
   class Controller extends app.Controller {
     async index(){
       await this.ctx.render('/admin/focus/index')
     }
     async multi(){}
-    async doSingleUpload(){}
+    async doSingleUpload(){
+      const stream = await this.ctx.getFileStream();
+      // 目标文件
+      const target = 'app/public/admin/upload/' + path.basename(stream.filename);
+      const writeStream = fs.createWriteStream(target);
+      await pump(stream,writeStream); 
+      this.ctx.body = {
+        url: target,      
+        fields: stream.fields      //表单的其他数据
+      }  
+    }
     async doMultiUpload(){}
   }
   return Controller
