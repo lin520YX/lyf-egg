ea030518ec59c5fa51131976449f91b3bb171315
林云富 <lin13177896353@163.com>
2021-12-28 23:50:26 +0800

    pump

--- egg-store/app/controller/admin/focus.js
+++ egg-store/app/controller/admin/focus.js
@@ -11,11 +11,14 @@ const fs=require('fs');
       await this.ctx.render('/admin/focus/multi')
     }
     async doSingleUpload(){
+      // enctype="multipart/form-data" 不能使用this.ctx.body.request 来接收post的数据
       const stream = await this.ctx.getFileStream();
+      console.log(stream)
       // 目标文件 创建一个空文件
       const target = 'app/public/admin/upload/' + path.basename(stream.filename);
       const writeStream = fs.createWriteStream(target);
       // 写入文件内容
+      // 如果失败可以关闭流 不然浏览器会卡死
       await pump(stream,writeStream); 
       this.ctx.body = {
         url: target,      
